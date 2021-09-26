0d6fe786d16ed1fd776f416f7e117974b72eeacb
Deric Abrantes <90254836+DericAbrantes@users.noreply.github.com>
2021-09-26 16:13:52 -0300

    Exemplo 2

--- pratica08/main.dart
+++ pratica08/main.dart
@@ -10,22 +10,25 @@ class Home extends StatelessWidget {
         leading: Icon(Icons.home),
         title: Text('Pagina Inicial'),
       ),
-      backgroundColor: Colors.blue[900],
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Exemplo de Botão',
-        child: Icon(Icons.add),
-      ),
-      body: const Center(
-        child: Text(
-          'Olá Mundo!!!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-}
+          
+            body: Center(
+              child: Container(
+              child: Text('Olá, Mundo!!!', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white)),
+              color: Colors.red,
+              alignment: Alignment.center,
+              width: 200,
+              height: 100,
+              ),
+            ),
+
+
+            backgroundColor: Colors.blue[900],
+            floatingActionButton: FloatingActionButton(
+            onPressed: null,
+            tooltip: 'Exemplo de botão',
+            child: Icon(Icons.add),
+            ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+            );
+            }
+            }
