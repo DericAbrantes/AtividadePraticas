de5241c24dab1db6245647fea1d875fc3173e0e3
Deric Abrantes <90254836+DericAbrantes@users.noreply.github.com>
2021-09-26 16:18:09 -0300

    Atividade pratica08

--- pratica08/main.dart
+++ pratica08/main.dart
@@ -10,6 +10,43 @@ class Home extends StatelessWidget {
         leading: Icon(Icons.home),
         title: Text('Pagina Inicial'),
       ),
+      backgroundColor: Colors.blue[900],
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Exemplo de Botão',
+        child: Icon(Icons.add),
+      ),
+      body: const Center(
+        child: Text(
+          'Olá Mundo!!!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+//EXEMPLO 2 ABAIXO !!!
+
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.home),
+        title: Text('Pagina Inicial'),
+      ),
           
             body: Center(
               child: Container(
